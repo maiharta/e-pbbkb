a2494a72f639b4653db43173a1a898543b2fa4c3
Aang Pangantyas Sampurna <aangps01@192.168.1.7>
2025-10-08 09:58:08 +0800

    change logo epbbkb

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -7,10 +7,10 @@
     <div class="text-center">
         @if($showTitle)
         <img src="{{ asset('assets/images/logo-provinsi.png') }}" alt="Logo Provinsi Bali" class="me-3" style="width: 60px; height: auto;">
-        <div>
+        {{-- <div>
             <h4 class="mb-0 fw-bold text-primary">BAPENDA</h4>
             <p class="mb-0 text-muted small">Provinsi Bali</p>
-        </div>
+        </div> --}}
     @endif
     </div>
     <img src="{{ asset('assets/images/logo.png') }}" alt="logo" width="{{ $width }}">
