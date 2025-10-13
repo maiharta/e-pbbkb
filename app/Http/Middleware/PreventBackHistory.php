5a8b411f4963b11d854f39f5d2306df90305a147
Aang Pangantyas Sampurna <aangps01@192.168.1.2>
2025-10-13 14:03:10 +0800

    hotfix

--- app/Http/Middleware/PreventBackHistory.php
+++ app/Http/Middleware/PreventBackHistory.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class PreventBackHistory
 {
@@ -17,11 +18,18 @@ class PreventBackHistory
     {
         $response = $next($request);
 
-        return $response->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
-            ->header('Pragma', 'no-cache')
-            ->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT')
-            ->header('Last-Modified', gmdate('D, d M Y H:i:s') . ' GMT')
-            ->header('Cache-Control', 'no-cache, must-revalidate, no-store, max-age=0, private')
-            ->header('X-Accel-Expires', '0');
+        // Skip cache prevention headers for file downloads
+        if ($response instanceof \Symfony\Component\HttpFoundation\BinaryFileResponse) {
+            return $response;
+        }
+
+        // Add cache prevention headers for regular responses
+        $response->headers->set('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+        $response->headers->set('Last-Modified', gmdate('D, d M Y H:i:s') . ' GMT');
+        $response->headers->set('X-Accel-Expires', '0');
+
+        return $response;
     }
 }
