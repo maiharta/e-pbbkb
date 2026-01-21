25c170fd06e3a296c4fe04ed49f3cc97998ef715
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2026-01-21 15:03:15 +0800

    hotfix

--- resources/views/pages/admin/verifikasi/pelaporan/show.blade.php
+++ resources/views/pages/admin/verifikasi/pelaporan/show.blade.php
@@ -459,12 +459,6 @@
         }
 
         function valid() {
-            // @if ($pelaporan->pelaporanNote->where('is_active', true)->where('status', 'danger')->count() != 0)
-            //     Swal.fire({
-            //         icon: 'error',
-            //         title: 'Gagal menyetujui laporan. Terdapat data yang belum sesuai dengan sistem'
-            //     });
-            // @else
                 Swal.fire({
                     icon: 'question',
                     title: 'Apakah anda yakin ingin menyetujui permohonan ini?',
@@ -505,7 +499,6 @@
                         }
                     });
                 });
-            // @endif
         }
     </script>
 @endpush
