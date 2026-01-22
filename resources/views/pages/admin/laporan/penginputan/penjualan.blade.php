0966af6cdd32b0e35073412e64c4d08011f2b692
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2026-01-23 00:58:14 +0800

    add laporan admin on penjualan table serverside

--- resources/views/pages/admin/laporan/penginputan/penjualan.blade.php
+++ resources/views/pages/admin/laporan/penginputan/penjualan.blade.php
@@ -6,12 +6,15 @@
             <div class="row">
                 <div class="col-12 col-md-6">
                     <h3>Data Penjualan</h3>
-                    <p class="text-muted">{{ $pelaporan->user->name }} - {{ $pelaporan->bulan_name }} {{ $pelaporan->tahun }}</p>
+                    <p class="text-muted">{{ $pelaporan->user->name }} - {{ $pelaporan->bulan_name }} {{ $pelaporan->tahun }}
+                    </p>
                 </div>
                 <div class="col-12 col-md-6">
-                    <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <nav aria-label="breadcrumb"
+                         class="breadcrumb-header float-start float-lg-end">
                         <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="{{ route('laporan.penginputan.index') }}">Laporan Penginputan</a></li>
+                            <li class="breadcrumb-item"><a href="{{ route('laporan.penginputan.index') }}">Laporan
+                                    Penginputan</a></li>
                             <li class="breadcrumb-item active">Data Penjualan</li>
                         </ol>
                     </nav>
@@ -23,36 +26,24 @@
             <div class="card">
                 <div class="card-body">
                     <div class="table-responsive">
-                        <table class="table table-striped table-hover" id="penjualan-table">
+                        <table class="table table-striped table-hover"
+                               id="penjualan-table">
                             <thead>
                                 <tr>
                                     <th>No</th>
-                                    <th>Invoice</th>
-                                    <th>Nama Pembeli</th>
-                                    <th>Sektor</th>
-                                    <th>Jenis BBM</th>
-                                    <th>Volume (Liter)</th>
-                                    <th>DPP</th>
-                                    <th>PBBKB</th>
+                                    <th>Pembeli</th>
+                                    <th>Nomor Kuitansi</th>
                                     <th>Tanggal</th>
-                                    <th>Lokasi Penyaluran</th>
+                                    <th>Jenis BBM</th>
+                                    <th>Sektor</th>
+                                    <th>Total Volume (liter)</th>
+                                    <th>DPP</th>
+                                    <th>Status Pajak</th>
+                                    <th>PBBKB User</th>
+                                    <th>PBBKB Sistem</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($penjualans as $penjualan)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $penjualan->nomor_kuitansi }}</td>
-                                        <td>{{ $penjualan->pembeli }}</td>
-                                        <td>{{ $penjualan->sektor->nama }}</td>
-                                        <td>{{ $penjualan->jenisBbm->nama }}</td>
-                                        <td class="text-end">{{ number_format($penjualan->volume, 0, ',', '.') }}</td>
-                                        <td class="text-end">Rp {{ number_format($penjualan->dpp, 2, ',', '.') }}</td>
-                                        <td class="text-end">Rp {{ number_format($penjualan->pbbkb_sistem, 2, ',', '.') }}</td>
-                                        <td>{{ Carbon\Carbon::parse($penjualan->tanggal)->format('d/m/Y') }}</td>
-                                        <td>{{ $penjualan->lokasi_penyaluran }}</td>
-                                    </tr>
-                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -65,9 +56,72 @@
 @push('scripts')
     <script>
         $('#penjualan-table').DataTable({
+            "responsive": true,
+            "processing": true,
+            "serverSide": true,
+            ajax: {
+                url: '{{ route('verifikasi.pelaporan.penjualan.table', $pelaporan->ulid) }}',
+                data: function(d) {
+                    d.search = $('#dt-search-0').val();
+                }
+            },
             "language": {
-                "url": '{{ asset("assets/vendors/datatables-lang-id.json") }}'
-            }
+                "url": '{{ asset('assets/vendors/datatables-lang-id.json') }}'
+            },
+            "columns": [{
+                    data: null,
+                    name: 'index',
+                    searchable: false,
+                    orderable: false,
+                    className: 'text-center',
+                    render: function(data, type, row, meta) {
+                        return meta.row + meta.settings._iDisplayStart + 1;
+                    }
+                },
+                {
+                    data: 'pembeli',
+                    name: 'pembeli',
+                    orderable: false
+                },
+                {
+                    data: 'nomor_kuitansi',
+                    name: 'nomor_kuitansi'
+                },
+                {
+                    data: 'tanggal',
+                    name: 'tanggal'
+                },
+                {
+                    data: 'jenis_bbm',
+                    name: 'jenis_bbm'
+                },
+                {
+                    data: 'sektor',
+                    name: 'sektor'
+                },
+                {
+                    data: 'volume',
+                    name: 'volume',
+                    className: 'text-end'
+                },
+                {
+                    data: 'dpp',
+                    name: 'dpp',
+                    className: 'text-end'
+                },
+                {
+                    data: "is_wajib_pajak",
+                    className: "text-start"
+                },
+                {
+                    data: "pbbkb",
+                    className: "text-start"
+                },
+                {
+                    data: "pbbkb_sistem",
+                    className: "text-start"
+                },
+            ],
         });
     </script>
 @endpush
