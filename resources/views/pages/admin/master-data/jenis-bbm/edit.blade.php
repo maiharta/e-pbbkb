2d6b713483a7c6ca8cd51978da287f32bc5620ff
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2026-02-09 08:56:52 +0800

    fix: edit jenis bbm when subsidi is 0

--- resources/views/pages/admin/master-data/jenis-bbm/edit.blade.php
+++ resources/views/pages/admin/master-data/jenis-bbm/edit.blade.php
@@ -106,10 +106,8 @@
             placeholder: 'Pilih tipe subsidi',
         });
 
-        @dd('is_subsidi', $jenis_bbm->is_subsidi)
-
         @if (!is_null(old('is_subsidi', $jenis_bbm->is_subsidi)))
-            $('#is_subsidi').val('{{ old('is_subsidi', $jenis_bbm->is_subsidi) == 0 ? '' : old('is_subsidi', $jenis_bbm->is_subsidi) }}').trigger('change');
+            $('#is_subsidi').val('{{ old('is_subsidi', $jenis_bbm->is_subsidi) == false ? '0' : old('is_subsidi', $jenis_bbm->is_subsidi) }}').trigger('change');
         @endif
     </script>
 @endpush
